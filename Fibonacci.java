97abe145e4745da1229fb6f6546f8f6599612804
anjali-dange <anjalidange9370@gmail.com>
2026-01-22 23:40:36 -0800

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Fibonacci{
+    public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+int n=sc.nextInt();
+int a=0,b=1,c;
+for(int i=1;i<=n;i++){
+System.out.println(a+"  ");
+
+c=a+b;
+a=b;
+b=c;   
+ }
+}
+}
